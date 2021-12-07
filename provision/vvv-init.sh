af4cc7ae3bd7f0c19c013ad7506bcfc7b372e0cd
Joshua (Personal) <josh@joshua-ThinkPad-X1-Carbon-5th>
2021-12-06 23:19:47 -0500

    Updated vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -30,3 +30,60 @@ if [ ! -z "${PUBLIC_DIR}" ]; then
   PUBLIC_DIR_PATH="${PUBLIC_DIR_PATH}/${PUBLIC_DIR}"
 fi
 echo -e $PUBLIC_DIR_PATH;
+
+# Make a database, if we don't already have one
+setup_database() {
+  echo -e " * Creating database '${DB_NAME}' (if it's not already there)"
+  mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`"
+  echo -e " * Granting the wp user priviledges to the '${DB_NAME}' database"
+  mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO wp@localhost IDENTIFIED BY 'wp';"
+  echo -e " * DB operations done."
+}
+
+setup_nginx_folders() {
+  echo " * Setting up the log subfolder for Nginx logs"
+  noroot mkdir -p "${VVV_PATH_TO_SITE}/log"
+  noroot touch "${VVV_PATH_TO_SITE}/log/nginx-error.log"
+  noroot touch "${VVV_PATH_TO_SITE}/log/nginx-access.log"
+  echo " * Creating the public folder at '${PUBLIC_DIR}' if it doesn't exist already"
+  noroot mkdir -p "${PUBLIC_DIR_PATH}"
+}
+
+copy_nginx_configs() {
+  echo " * Copying the sites Nginx config template"
+  if [ -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" ]; then
+    echo " * A vvv-nginx-custom.conf file was found"
+    noroot cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  else
+    echo " * Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
+    noroot cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-default.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  fi
+
+  echo " * Applying public dir setting to Nginx config"
+  noroot sed -i "s#{vvv_public_dir}#/${PUBLIC_DIR}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+  LIVE_URL=$(get_config_value 'live_url' '')
+  if [ ! -z "$LIVE_URL" ]; then
+    echo " * Adding support for Live URL redirects to NGINX of the website's media"
+    # replace potential protocols, and remove trailing slashes
+    LIVE_URL=$(echo "${LIVE_URL}" | sed 's|https://||' | sed 's|http://||'  | sed 's:/*$::')
+
+    redirect_config=$((cat <<END_HEREDOC
+if (!-e \$request_filename) {
+  rewrite ^/[_0-9a-zA-Z-]+(/app/uploads/.*) \$1;
+}
+if (!-e \$request_filename) {
+  rewrite ^/app/uploads/(.*)\$ \$scheme://${LIVE_URL}/app/uploads/\$1 redirect;
+}
+END_HEREDOC
+
+    ) |
+    # pipe and escape new lines of the HEREDOC for usage in sed
+    sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n\\1/g'
+    )
+
+    noroot sed -i -e "s|\(.*\){{LIVE_URL}}|\1${redirect_config}|" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  else
+    noroot sed -i "s#{{LIVE_URL}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  fi
+}
