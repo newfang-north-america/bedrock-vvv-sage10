3d01cc74e7b611564138b5f91e444c6a9a93e272
Joshua (Personal) <josh@joshua-ThinkPad-X1-Carbon-5th>
2021-12-06 22:39:11 -0500

    initial vvv-init.sh changes

---
+++ provision/vvv-init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Provision WordPress Stable
+
+set -eo pipefail
+
+echo " * Custom site template provisioner ${VVV_SITE_NAME} - downloads and installs a copy of WP stable for testing, building client sites, etc"
+
+# fetch the first host as the primary domain. If none is available, generate a default using the site name
+DB_NAME=$(get_config_value 'db_name' "${VVV_SITE_NAME}")
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*]/}
+DB_PREFIX=$(get_config_value 'db_prefix' 'wp_')
+DOMAIN=$(get_primary_host "${VVV_SITE_NAME}".test)
+PUBLIC_DIR=$(get_config_value 'public_dir' "public_html")
+SITE_TITLE=$(get_config_value 'site_title' "${DOMAIN}")
+WP_LOCALE=$(get_config_value 'locale' 'en_US')
+WP_TYPE=$(get_config_value 'wp_type' "single")
+WP_VERSION=$(get_config_value 'wp_version' 'latest')
+
+echo -e $DB_NAME;
+echo -e $DB_PREFIX;
+echo -e $DOMAIN;
+echo -e $PUBLIC_DIR;
+echo -e $SITE_TITLE;
+echo -e $WP_LOCALE;
+echo -e $WP_TYPE;
+echo -e $WP_VERSION;
